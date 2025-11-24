a13535de091ca7c5aa2c027991836cdde72215f0
Kanat <kanat2008.kn@gmail.com>
2025-11-24 23:37:25 +0500

    дома сделал

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let buttonText;
+let buttonText2 = "Скрыть текст";
+
+document.querySelector('.info__button').addEventListener('click', function () {
+    document.querySelector('.info__text').classList.toggle('info__text--active');
+    buttonText = this.textContent;
+    this.textContent = buttonText2;
+    buttonText2 =  buttonText;
+})
